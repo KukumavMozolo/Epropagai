f032cfe28c16e120b836e85c95057a16c3f87dc8
maxweule <mw@plista.com>
2016-12-16 12:52:10 +0100

    implemented methods

--- src/t0n.py
+++ src/t0n.py
@@ -1,42 +1,37 @@
 import numpy as np
-
+from scipy.stats import norm
 
 # Here are Methods for equation 17 - 21
 class t0n:
 
     def getMuNew(self, muOld: np.ndarray, vOld: np.ndarray, xi: np.ndarray, ai: float) -> np.ndarray: #17
-        return NotImplemented
-
+        return muOld + ai* np.multiply(vOld, xi)
 
     def getVNew(self, vOld: np.ndarray, xi: np.ndarray, muNew: np.ndarray, ai: float): #18
-        return NotImplemented
-
+        return vOld - ai * (np.dot(xi,muNew) + ai)/(np.dot(xi,np.multiply(vOld,xi)) +1) * np.multiply(vOld, xi) * np.multiply(vOld,xi)
 
     def getvViNew(self, vNew: np.ndarray, vOld: np.ndarray) -> np.ndarray:#19
-        return NotImplemented
-
+        return np.reciprocal(np.reciprocal(vNew) - np.reciprocal(vOld))
 
     def getMiNew(self, muOld: np.ndarray, viNew: np.ndarray, xi: np.ndarray, viOld: np.ndarray, ai: float) -> np.ndarray:#20
-        return NotImplemented
+        return muOld + ai*np.multiply(viNew, xi) + ai * np.multiply(viOld, xi)
 
-
-    def getSi(self, viNew: np.ndarray, vOld: np.ndarray, miNew: np.ndarray, muOld: np.ndarray) -> float:#21
-        return NotImplemented
+    def getSi(self, z:float, viNew: np.ndarray, vOld: np.ndarray, miNew: np.ndarray, muOld: np.ndarray) -> float:#21
+        #not sure about this
+        return norm.cdf(z) + np.prod(np.sqrt(np.divide(viNew + vOld, viNew))) * np.exp(np.sum(np.divide(np.power(miNew - muOld,2), 2 *(vOld + viNew))))
 
 
     #after where statement
 
     def getVOld(self, v: np.ndarray, vi: np.ndarray) -> np.ndarray: #22
-        return NotImplemented
-
+        return np.reciprocal(np.reciprocal(v) - np.reciprocal(vi))
 
     def getMuOld(self, mu: np.ndarray, vOld: np.ndarray, vi: np.ndarray, mi: np.ndarray) -> np.ndarray:#23
-        return NotImplemented
-
+        return mu + np.multiply(vOld, np.multiply(np.reciprocal(vi), mu-mi))
 
     def getAlphai(self, xi: np.ndarray, vOld: np.ndarray, z: float) -> float: #24
-        return NotImplemented
-
+        #maybe norm requires params?
+        return 1.0/np.sqrt(np.dot(xi,np.multiply(vOld,xi)) +1) * norm.pdf(z)/norm.cdf(z)
 
     def getZi(self, xi: np.ndarray, muOld: np.ndarray, vOld: np.ndarray) -> float: #25
-        return NotImplemented
+        return np.dot(xi,muOld)/np.sqrt(np.dot(xi, np.multiply(vOld,xi)) +1)
--- src/tn1d.py
+++ src/tn1d.py
@@ -1,42 +1,60 @@
 import numpy as np
 import t0n
+from scipy.stats import norm
 
 #here are equations 26 -42
 class tn1d:
     def getMuiNew(self, muiOld: float, c1:float, viOld:float) -> float: #26
-        return NotImplemented
+        return muiOld + c1*viOld
+
     def getViNew(self, viOld: float, c3:float)-> float: #27
-        return NotImplemented
+        return viOld - c3 *np.power(viOld,2)
+
     def getPiNew(self, piOld: float, g1: float, g0:float)->float:#28
-        return NotImplemented
+        return piOld*g1/piOld*g1 + (1-piOld)*g0
+
     def getvViNew(self, viOld: float, c3: float)->float: #29
-        return NotImplemented
+        return np.reciprocal(c3) - viOld
+
     def getmMiNew(self, muiOld: float, c1: float, vviNew:float, viOld: float) -> float: #30
-        return NotImplemented
+        return muiOld + c1*(vviNew + viOld)
+
     def getaAiNew(self, piNew: float, piOld:float)->float:#31
-        return NotImplemented
+        return piNew/piOld
+
     def getbBiNew(self, piNew: float, piOld: float)->float:#32
-        return NotImplemented
-    def getSi(self, viOld:float, vviiNew: float, c1: float, c3: float)-> float:#33 i think viiNew denotes the v comming from eq.19
-        return NotImplemented
+        return (1 - piNew)/(1 - piOld)
+
+    def getSi(self, z: float, viOld:float, vviiNew: float, c1: float, c3: float)-> float:#33 i think viiNew denotes the v comming from eq.19
+        return z * np.sqrt((viOld +vviiNew)/vviiNew) * np.exp(0.5 * np.power(c1,2)/c3 )
 
     #from after where
     def getViOld(self, vi: float, vvi) ->float: #34
-        return NotImplemented
+        return np.reciprocal(np.reciprocal(vi) - np.reciprocal(vvi))
+
     def getMuiOld(self, mui: float, viOld: float, vvi: float, mmi: float) ->float: #35
-        return NotImplemented
+        return mui + viOld *np.reciprocal(vvi) * (mui - mmi)
+
     def getZ(self, piOld: float, g1: float, g0: float) -> float: #36
-        return NotImplemented
-    def getC1(self, z: float, piOld: float, g1: float, muiOld: float, viOld: float, sigma1: float, sigma2: float)-> float: #37
-        return NotImplemented
-    def getC2(self, z: float, piOld: float, g1:float, g0:float, muiOld: float, viOld: float, sigma1: float, sigma2: float) -> float: #38
-        return NotImplemented
+        return piOld * g1 + (1 - piOld)*g0
+
+    def getC1(self, z: float, piOld: float, g0:float, g1: float, muiOld: float, viOld: float, sigma1: float, sigma0: float)-> float: #37
+        return np.reciprocal(z) * 0.5 *(piOld*g1 *(-muiOld)/(viOld + np.power(sigma1,2)) + (1.0-piOld) *g0 * (-muiOld)/(viOld + np.power(sigma0,2)))
+
+    def getC2(self, z: float, piOld: float, g1:float, g0:float, muiOld: float, viOld: float, sigma1: float, sigma0: float) -> float: #38
+        def helper(muiOld: float, viOld: float, sigma1: float):
+            return np.power(muiOld, 2) / np.power(viOld + np.power(sigma1,2)) - 1.0/(viOld + np.power(sigma1,2))
+        return np.reciprocal(z) * 0.5 * (piOld*g1*helper(muiOld, viOld, sigma1) + (1.0 -piOld) * g0 * helper(muiOld, viOld, sigma0))
+
     def getC3(self, c1: float, c2: float)-> float: #39
-        return NotImplemented
+        return np.power(c1,2) - 2*c2
+
     def getG0(self, muiOld: float, viOld: float, sigma0: float)->float: #40
-        return NotImplemented
+        return norm.pdf(0, muiOld, viOld + sigma0)
+
     def getG1(self, muiOld: float, viOld: float, sigma1: float)->float: #41
-        return NotImplemented
+        return norm.pdf(0, muiOld, viOld + sigma1)
+
     def getPiOld(self, pi: float, aai:float, bbi:float)->float: #42
-        return NotImplemented
+        return (pi/aai)/(pi/aai + (1.0 -pi)/bbi)
 
